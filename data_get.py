888966ffeeef084135f0c08858d058e8220cf709
taitokurssiGit <taitokurssiGit@users.noreply.github.com>
2020-09-23 21:04:01 +0300

    Correction: move data wrangling script one folder up.

---
+++ data_get.py
@@ -0,0 +1,14 @@
+def data_read() -> dict:
+    """This script is used to read data into python. The data is provided as a csv-file in a subfolder 'data'. 
+
+    Returns:
+        dict: A key is a year, a value is a float. 
+    """
+    import csv
+
+    with open('./data/data_raw.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+#        with open('data_raw_new.csv', mode='w') as outfile:
+#            writer = csv.writer(outfile)
+        data_dict = {int(rows[0]): float(rows[1]) for rows in reader}
+    return data_dict
