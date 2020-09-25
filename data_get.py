aab661153f26d6be45b7c17fbf72d8ae986cca2a
taitokurssiGit <taitokurssiGit@users.noreply.github.com>
2020-09-25 20:29:21 +0300

    Add try-except statements. Introduces a bug into the main function of data_get.py.

--- data_get.py
+++ data_get.py
@@ -6,9 +6,36 @@ def data_read() -> dict:
     """
     import csv
 
-    with open('./data/data_raw.csv', mode='r') as infile:
-        reader = csv.reader(infile)
-#        with open('data_raw_new.csv', mode='w') as outfile:
-#            writer = csv.writer(outfile)
-        data_dict = {int(rows[0]): float(rows[1]) for rows in reader}
-    return data_dict
+    try:
+        with open('./data/data_raw.csv', mode='r') as infile:
+            reader = csv.reader(infile)
+            data_dict = {int(rows[0]): float(rows[1]) for rows in reader}
+        return data_dict
+
+    except FileNotFoundError as file_error:
+        print(file_error, "The data could not be found. The program is closing.", sep='\n')
+
+    except:
+        import sys
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+
+
+def main():
+    try:
+        print("The raw data in dictionary format:", data_read(), sep='\n')
+# TODO: Fix so that the above line will NOT be printed if reading the csv file has failed. 
+
+    except NameError as name_error:
+        print(name_error, "The data could not be read into dictionary format. The program is closing.", sep='\n')
+
+    except:
+        import sys
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+
+
+if __name__ == '__main__':
+    main()
--- detector.py
+++ detector.py
@@ -1,22 +1,36 @@
-# Imports a function 'data_read' from a selfmade module 'data_get' residing in a subfolder 'data'. 
-# That function provides some data in a dictionary format into this main program. 
-from data_get import data_read
+# Imports a function 'data_read' from a module 'data_get'. 
+#                       TODO: ...from a module 'data_get' residing in a subfolder 'data'. 
 
-data_dict = data_read()
+try:
+    from data_get import data_read # or: from data.data_get import data_read
+    # This function provides some data in a dictionary format into this main program:
+    data_dict = data_read()
 
-print("Testing the data by printing the value for the year 2000: ", data_dict[2000], sep='\n')
+except ModuleNotFoundError as import_e:
+    print(import_e, sep='\n')
 
-#TODO: code the following functionally without while loop. 
+except FileNotFoundError as file_error:
+    print(file_error, """Check that your current folder is the same where this detector.py script is located. 
+    Launch the program there by commanding
+    python detector.py""", sep='\n')
 
-while True:
-    x = int(input("Enter a year between 1901–2020: ")) # TODO: 1) Make tests for possible malformated inputs. 2) Remove hard coded years. 
-    if data_dict[x] - data_dict[x - 1] > 0:
-        print(f"It got BIGGER between {x - 1}–{x}.") # TODO: print also some values (compare also to x-10), e.g. increase in percentage: lambda x: ((data_dict[x] - data_dict[x - 1]) / data_dict[x - 1]) * 100
-    else:
-        print(f"That was extremely lucky! It got smaller between {x - 1}–{x}.")
-    
-    again = input("Do you want to test another year? (Y/N): ")
+except:
+    import sys
+    print("Unexpected error:", sys.exc_info()[0])
+    raise    
 
-    if again.lower() == "n":
-        print("That's the spirit. Good bye!")
-        break
+else:
+    #TODO: code the following functionally without while loop?
+
+    while True:
+        x = int(input("Enter a year between 1901–2020: ")) # TODO: 1) Make tests for possible malformated inputs. 2) Remove hard coded years. 
+        if data_dict[x] - data_dict[x - 1] > 0:
+            print(f"It got BIGGER between {x - 1}–{x}.") # TODO: print also some values (compare also to x-10), e.g. increase in percentage: lambda x: ((data_dict[x] - data_dict[x - 1]) / data_dict[x - 1]) * 100
+        else:
+            print(f"That was extremely lucky! It got smaller between {x - 1}–{x}.")
+        
+        again = input("Do you want to test another year? (Y/N): ")
+
+        if again.lower() == "n":
+            print("That's the spirit. Good bye!")
+            break
